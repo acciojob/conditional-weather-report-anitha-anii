ff277a9ffc35068bbff304b3711ed6cc10057946
anitha-anii <anithachowdary355@gmail.com>
2023-07-15 17:55:06 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
 import React from "react";
-import WeatherApp from "./WeatherApp.js";
 import './../styles/App.css';
 
-const App = () => {
-   const weatherData = { temperature: 25, conditions: "Sunny" };
 
-  return (
-    <div id="main">
-      <WeatherApp weather={weatherData} />
-    </div>
-  )
-   
+class App  extends React.Component {
+    state = {
+    weather: {
+      temperature: 25,
+      conditions: "Sunny",
+    }
+  };
+  threshold = 20;
+
+  render() {
+    const { weather } = this.state;
+    const temperatureColor = weather.temperature > this.threshold ? "red" : "blue";
+
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <p>Current Temperature: <span style={{ color: temperatureColor }}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
+      </div>
+    );
+  }
 }
+
 
 export default App
